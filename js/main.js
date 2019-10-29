9a47e4910cd6f913e0721ba37f60e00869a05ed1
anti-matt-er <m.burrows@live.co.uk>
2019-10-29 20:43:38 +0000

    Accessibility stuff and A11Y compliancy

--- js/main.js
+++ js/main.js
@@ -11,12 +11,14 @@ const section_nav_buttons = document.querySelectorAll(".section-nav a");
 const contact_form = document.querySelector(".contact-form");
 const contact_button = document.querySelectorAll(".contact");
 const contact_close = document.querySelector(".contact-form .close");
+const contact_tabbables = document.querySelectorAll(".contact-form input, .contact-form textarea, .contact-form button");
 const parallax_divider = 8;
 const parallax_lerp_divider = 16;
 const parallax_lerp = 1 / parallax_lerp_divider;
 const parallax_threshold = 50;
 const section_nav_scroll_threshold = 100;
 const default_section_nav_desc = section_nav.getAttribute("data-description");
+const no_motion_query = window.matchMedia('(prefers-reduced-motion: reduce)');
 
 /** VARIABLES **/
 
@@ -29,11 +31,16 @@ var contact_open = false;
 var blank_section_nav = false;
 var using_tilt = false;
 var landing = true;
+var no_motion = false;
 
 /** INIT **/
 
 window.scroll({
   top: 0,
+});
+
+no_motion_query.addEventListener('change', () => {
+  no_motion = true;
 });
 
 Array.prototype.forEach.call(odd_sections, function(el, i) {
@@ -87,6 +94,11 @@ Array.prototype.forEach.call(contact_button, function(el, i) {
   el.addEventListener("click", function (e) {
     contact_open = true;
     contact_form.classList.remove("modal-closed");
+    contact_form.focus();
+    contact_form.setAttribute("tabindex", "0");
+    Array.prototype.forEach.call(contact_tabbables, function(c_el, c_i) {
+      c_el.setAttribute("tabindex", "0");
+    });
     requestAnimationFrame(update);
   });
 });
@@ -94,6 +106,11 @@ Array.prototype.forEach.call(contact_button, function(el, i) {
 contact_close.addEventListener("click", function (e) {
   contact_open = false;
   contact_form.classList.add("modal-closed");
+  contact_form.setAttribute("tabindex", "-1");
+  Array.prototype.forEach.call(contact_tabbables, function(c_el, c_i) {
+    c_el.setAttribute("tabindex", "-1");
+  });
+  contact_button[0].focus();
   requestAnimationFrame(update);
 });
 
@@ -140,16 +157,19 @@ function update() {
 /** FUNCTIONS **/
 
 function update_pattern() {
-  var currentX = parseFloat(container.style.backgroundPositionX);
-  var currentY = parseFloat(container.style.backgroundPositionY);
-  var mouse_y = mouse_pos.y;
-  if (!using_tilt)
+  if (!no_motion)
   {
-     mouse_y = -(mouse_y + scroll_pos);
+    var currentX = parseFloat(container.style.backgroundPositionX);
+    var currentY = parseFloat(container.style.backgroundPositionY);
+    var mouse_y = mouse_pos.y;
+    if (!using_tilt)
+    {
+       mouse_y = -(mouse_y + scroll_pos);
+    }
+    container.style.backgroundPositionX = lerp(currentX, -mouse_pos.x / parallax_divider, parallax_lerp) + "px";
+    container.style.backgroundPositionY = lerp(currentY, mouse_y / parallax_divider, parallax_lerp) + "px";
+    requestAnimationFrame(update_pattern);
   }
-  container.style.backgroundPositionX = lerp(currentX, -mouse_pos.x / parallax_divider, parallax_lerp) + "px";
-  container.style.backgroundPositionY = lerp(currentY, mouse_y / parallax_divider, parallax_lerp) + "px";
-  requestAnimationFrame(update_pattern);
 }
 
 function lerp(start, end, amt) {
